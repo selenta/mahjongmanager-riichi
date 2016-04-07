6a482e42aa62145432374ee07d8795cf521a9ef4
Evan Nelson <selentian@gmail.com>
2016-04-07 14:34:31 -0700

    SpeedQuiz start screen now displays high score

--- app/src/main/java/com/mahjongmanager/riichi/speedquiz/SpeedQuizFragment_1Start.java
+++ app/src/main/java/com/mahjongmanager/riichi/speedquiz/SpeedQuizFragment_1Start.java
@@ -1,16 +1,29 @@
 package com.mahjongmanager.riichi.speedquiz;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.mahjongmanager.riichi.R;
 
 public class SpeedQuizFragment_1Start extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_speedquiz_1start, container, false);
+        View myInflatedView = inflater.inflate(R.layout.fragment_speedquiz_1start, container, false);
+
+        TextView currentHighScoreLabel = (TextView) myInflatedView.findViewById(R.id.speedQuizHighScore);
+
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        Integer highScore = sharedPref.getInt("SpeedQuizHighScore", 0);
+
+        String highScoreLabel = "Current Record: " + highScore;
+        currentHighScoreLabel.setText(highScoreLabel);
+
+        return myInflatedView;
     }
 }
