187679696d56f65a34105094bb4f964551173f6d
Evan Nelson <selentian@gmail.com>
2016-05-04 14:04:05 -0700

    Loads images for HandDisplay upon opening app.

--- app/src/main/java/com/mahjongmanager/riichi/MainActivity.java
+++ app/src/main/java/com/mahjongmanager/riichi/MainActivity.java
@@ -60,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         getExampleHands();
+        getImageCache();
     }
     ////////////////////////////////////////////
     /////////////    General     ///////////////
