e58e2c7b3e09837b11c1cf49923f542884e64d01
Evan Nelson <selentian@gmail.com>
2017-02-06 18:05:48 -0800

    Re-added control to Log levels, minor cleanup.

--- app/src/main/java/com/mahjongmanager/riichi/utils/Log.java
+++ app/src/main/java/com/mahjongmanager/riichi/utils/Log.java
@@ -3,38 +3,40 @@ package com.mahjongmanager.riichi.utils;
 import com.mahjongmanager.riichi.BuildConfig;
 
 public class Log {
+    private static int LOG_LEVEL = 3;
+
     public static void v(String tag, String msg){
-        if(BuildConfig.DEBUG) {
+        if(BuildConfig.DEBUG && LOG_LEVEL>=5) {
             android.util.Log.v( tag, msg );
         }
     }
 
     public static void d(String tag, String msg){
-        if(BuildConfig.DEBUG) {
+        if(BuildConfig.DEBUG && LOG_LEVEL>=4) {
             android.util.Log.d( tag, msg );
         }
     }
 
     public static void i(String tag, String msg){
-        if(BuildConfig.DEBUG) {
+        if(BuildConfig.DEBUG && LOG_LEVEL>=3) {
             android.util.Log.i( tag, msg );
         }
     }
 
     public static void w(String tag, String msg){
-        if(BuildConfig.DEBUG) {
+        if(BuildConfig.DEBUG && LOG_LEVEL>=2) {
             android.util.Log.w( tag, msg );
         }
     }
 
     public static void e(String tag, String msg){
-        if(BuildConfig.DEBUG) {
+        if(BuildConfig.DEBUG && LOG_LEVEL>=1) {
             android.util.Log.e( tag, msg );
         }
     }
 
     public static void wtf(String tag, String msg){
-        if(BuildConfig.DEBUG) {
+        if(BuildConfig.DEBUG && LOG_LEVEL>=0) {
             android.util.Log.wtf( tag, msg );
         }
     }
