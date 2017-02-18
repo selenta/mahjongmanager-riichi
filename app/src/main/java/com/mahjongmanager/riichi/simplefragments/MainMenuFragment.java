c0ecc62d73233f37c139e41b53a41054c390f0c8
Evan Nelson <selentian@gmail.com>
2017-02-17 18:04:32 -0800

    MainMenu now has some color, and a more interesting title

--- app/src/main/java/com/mahjongmanager/riichi/simplefragments/MainMenuFragment.java
+++ app/src/main/java/com/mahjongmanager/riichi/simplefragments/MainMenuFragment.java
@@ -5,23 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.mahjongmanager.riichi.R;
-import com.mahjongmanager.riichi.utils.Utils;
 
 public class MainMenuFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View myInflatedView = inflater.inflate(R.layout.fragment_mainmenu, container, false);
-
-        //Update title to fit smaller screens
-        if( Utils.SCREEN_SIZE < 1024 ){
-            TextView appTitleLabel = (TextView) myInflatedView.findViewById(R.id.appTitleLabel);
-            appTitleLabel.setTextSize(appTitleLabel.getTextSize()*0.55f);
-        }
-
-        return myInflatedView;
+        return inflater.inflate(R.layout.fragment_mainmenu, container, false);
     }
 }
