71685032503dbb4ccf2e0cbbf97737a78107761c
Evan Nelson <selentian@gmail.com>
2017-03-04 05:58:52 -0800

    Cleanup and updte build settings for release version. MainMenu now displays real version number.

--- app/src/main/java/com/mahjongmanager/riichi/simplefragments/MainMenuFragment.java
+++ app/src/main/java/com/mahjongmanager/riichi/simplefragments/MainMenuFragment.java
@@ -1,10 +1,13 @@
 package com.mahjongmanager.riichi.simplefragments;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.mahjongmanager.riichi.R;
 
@@ -12,6 +15,18 @@ public class MainMenuFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_mainmenu, container, false);
+        View myInflatedView = inflater.inflate(R.layout.fragment_mainmenu, container, false);
+
+        String versionName = "v";
+        try {
+            PackageInfo pi = getContext().getPackageManager().getPackageInfo(getContext().getPackageName(), 0);
+            versionName += pi.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        TextView versionLabel = (TextView) myInflatedView.findViewById(R.id.appVersionLabel);
+        versionLabel.setText(versionName);
+
+        return myInflatedView;
     }
 }
